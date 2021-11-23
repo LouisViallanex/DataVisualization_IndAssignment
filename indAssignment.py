d1014b0240bc34059a534326e87f16ca91c7d0ed
Louis <louisviallanex@gmail.com>
2021-11-23 20:05:58 +0100

    up

--- indAssignment.py
+++ indAssignment.py
@@ -28,7 +28,7 @@ usagers_decede = usagers.copy()
 
 usagers_decede = usagers_decede.drop(usagers_decede[usagers_decede.grav != 2].index)
 
-print(usagers_decede)
+#print(usagers_decede)
 
 #print(100 * sum(usagers['grav'] == 2) / len(usagers)) # Pourcentage des personnes impliquées dans un accident qui sont mortes
 
@@ -40,4 +40,44 @@ print(100 * len(usagers_decede['Num_Acc'].unique()) / len(usagers['Num_Acc'].uni
 
 # Repartition par age 
 
-print(usagers)
+#print(usagers)
+
+ageAccidenté = dict.fromkeys(range(130), 0)
+ageMort = dict.fromkeys(range(130), 0)
+ageAccidenté_prct = dict.fromkeys(range(130), 0)
+ageMort_prct = dict.fromkeys(range(130), 0)
+
+#print(ageAccidenté)
+
+#print(sum(usagers['an_nais']<1921))
+
+for index, row in usagers.iterrows():
+    ageAccidenté[2021-row['an_nais']] += 1
+    
+for index, row in usagers_decede.iterrows():
+    ageMort[2021-row['an_nais']] += 1
+
+for i in ageAccidenté.keys():
+    ageAccidenté_prct[i] = ageAccidenté[i] / sum(ageAccidenté.values())
+for i in ageMort.keys():
+    ageMort_prct[i] = ageMort[i] / sum(ageMort.values())
+
+#ageAccidenté_prct = ageAccidenté/sum(ageAccidenté)
+#ageMort_prct = ageMort/sum(ageMort)
+
+#print(ageAccidenté)
+
+fig, axs = plt.subplots(ncols=2)
+#ax.bar(ageAccidenté.keys(), ageAccidenté.values())
+#plt.show()
+
+#ax.bar(ageMort.keys(), ageMort.values())
+
+axs[0].bar(ageAccidenté.keys(),    ageAccidenté.values(),  color='orange')
+axs[0].bar(ageMort.keys(),         ageMort.values(),       color='red')
+
+axs[1].bar(ageAccidenté_prct.keys(),    ageAccidenté_prct.values(),  color='orange')
+axs[1].bar(ageMort_prct.keys(),         ageMort_prct.values(),       color='red')
+
+
+plt.show()
